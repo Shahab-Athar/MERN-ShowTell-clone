73d4905a6e8190458308af55aa3b80da6c589971
Sajawal Hassan <scientificninja1f12@gmail.com>
2021-04-23 19:41:39 +0500

    add btns to swipebuttons

--- tinder-clone/src/SwipeButtons.js
+++ tinder-clone/src/SwipeButtons.js
@@ -2,14 +2,28 @@ import React from 'react'
 import ReplayIcon from "@material-ui/icons/Replay"
 import CloseIcon from "@material-ui/icons/Close"
 import StartRateIcon from "@material-ui/icons/StarRate"
-import FaveriteIcon from "@material-ui/icons/Favorite"
+import FavoriteIcon from "@material-ui/icons/Favorite"
 import FlasOnIcon from "@material-ui/icons/FlashOn"
 import IconButton from "@material-ui/core/IconButton"
 
 function SwipeButtons() {
     return (
         <div className="swipeButtons">
-            <h1>Hello</h1>
+            <IconButton>
+                <ReplayIcon fontSize="large" />
+            </IconButton>
+            <IconButton>
+                <CloseIcon fontSize="large" />
+            </IconButton>
+            <IconButton>
+                <StartRateIcon fontSize="large" />
+            </IconButton>
+            <IconButton>
+                <FavoriteIcon fontSize="large" />
+            </IconButton>
+            <IconButton>
+                <FlasOnIcon fontSize="large" />
+            </IconButton>
         </div>
     )
 }
