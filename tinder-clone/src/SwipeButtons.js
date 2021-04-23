79ce3365bbb6fc26195e525e99a0a6594931cded
Sajawal Hassan <scientificninja1f12@gmail.com>
2021-04-23 19:59:37 +0500

    Add Style To Footer Btns

--- tinder-clone/src/SwipeButtons.js
+++ tinder-clone/src/SwipeButtons.js
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import ReplayIcon from "@material-ui/icons/Replay"
 import CloseIcon from "@material-ui/icons/Close"
 import StartRateIcon from "@material-ui/icons/StarRate"
@@ -6,22 +7,25 @@ import FavoriteIcon from "@material-ui/icons/Favorite"
 import FlasOnIcon from "@material-ui/icons/FlashOn"
 import IconButton from "@material-ui/core/IconButton"
 
+import './SwipeButtons.css'
+
+
 function SwipeButtons() {
     return (
         <div className="swipeButtons">
-            <IconButton>
+            <IconButton className="swipeButtons_repeat">
                 <ReplayIcon fontSize="large" />
             </IconButton>
-            <IconButton>
+            <IconButton className="swipeButtons_left">
                 <CloseIcon fontSize="large" />
             </IconButton>
-            <IconButton>
+            <IconButton className="swipeButtons_star">
                 <StartRateIcon fontSize="large" />
             </IconButton>
-            <IconButton>
+            <IconButton className="swipeButtons_right">
                 <FavoriteIcon fontSize="large" />
             </IconButton>
-            <IconButton>
+            <IconButton className="swipeButtons_lightning">
                 <FlasOnIcon fontSize="large" />
             </IconButton>
         </div>
