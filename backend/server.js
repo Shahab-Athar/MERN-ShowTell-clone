9e28a54d077378087e16a4f452356835a8848e25
Shahab-Athar <shahabathar1039@gmail.com>
2021-04-24 15:13:01 +0500

    Made First Api Endpoint

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 
 import password from './config.js'
+import Cards from './dbCards.js '
 
 // Config
 const app = express()
@@ -17,6 +18,29 @@ mongoose.connect(connection_url, {
 
 // API Endpoint
 app.get("/", (req, res) => res.status(200).send("Hello World"))
+
+app.post('/showtell/cards', (req, res) => {
+    const dbcards = req.body
+
+    Cards.create(dbCard, (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(data)
+        }
+    })
+})
+
+app.get ('/showtell/cards', (req, res) => {
+
+    Cards.find((err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(data)
+        }
+    })
+})
 
 // Listener
 app.listen(port, () => console.log(`Listenning On Local Host http://localhost:${port}`))
