3638b832081c93827b576d588e04674701b5b68a
Shahab-Athar <shahabathar1039@gmail.com>
2021-04-24 14:32:49 +0500

    Connect Database

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,20 @@ import mongoose from 'mongoose'
 
 import password from './config.js'
 
+// Config
 const app = express()
 const port = process.env.PORT || 8001
 const connection_url = `mongodb+srv://admin:${password}@cluster0.co4vh.mongodb.net/ShowTelldb?retryWrites=true&w=majority`
 
+// DB Config
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
+
+// API Endpoint
 app.get("/", (req, res) => res.status(200).send("Hello World"))
 
+// Listener
 app.listen(port, () => console.log(`Listenning On Local Host http://localhost:${port}`))
-
-// LLa4v80RhBZhSVBx
